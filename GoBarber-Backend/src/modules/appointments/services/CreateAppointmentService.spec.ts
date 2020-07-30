042b4b3f2f23c85d7c9e243718e1e9e0b77f4178
GermainPereira <pereira.germain@outlook.com>
2020-07-30 09:26:52 -0300

    Added test for repeated appointment

--- GoBarber-Backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ GoBarber-Backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,6 @@
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateAppointment', () => {
     it('should be able to create a new appointment', async () => {
@@ -16,5 +17,27 @@ describe('CreateAppointment', () => {
 
         expect(appointment).toHaveProperty('id');
         expect(appointment.provider_id).toBe('12312313');
+    });
+
+    it('should not be able to two appointments at the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '12312313',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '12312313',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
