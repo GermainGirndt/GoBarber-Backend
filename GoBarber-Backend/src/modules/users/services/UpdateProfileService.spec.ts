9955665ab842637915761e15fb3e894b04e927b9
GermainPereira <pereira.germain@outlook.com>
2020-08-12 08:02:38 -0300

    Added update profile features

---
+++ GoBarber-Backend/src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,114 @@
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import UpdateProfileService from './UpdateProfileService';
+
+import AppError from '@shared/errors/AppError';
+
+let fakeHashProvider: FakeHashProvider;
+let fakeUsersRepository: FakeUsersRepository;
+let updateProfileService: UpdateProfileService;
+
+describe('Update Profile', () => {
+    beforeEach(() => {
+        fakeHashProvider = new FakeHashProvider();
+        fakeUsersRepository = new FakeUsersRepository();
+
+        updateProfileService = new UpdateProfileService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+    });
+
+    it('should be able to update the profile', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const updatedUser = await updateProfileService.execute({
+            userId: user.id,
+            name: 'John Trê',
+            email: 'johntre@example.com',
+            oldPassword: '123456',
+        });
+
+        expect(updatedUser.name).toBe('John Trê');
+        expect(updatedUser.email).toBe('johntre@example.com');
+    });
+
+    it('should be able to reuse a previous email', async () => {
+        await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const user = await fakeUsersRepository.create({
+            name: 'John Trê',
+            email: 'johntre@example.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfileService.execute({
+                userId: user.id,
+                name: 'John Três',
+                email: 'johndoe@example.com',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should be able to update the password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const updatedUser = await updateProfileService.execute({
+            userId: user.id,
+            name: 'John Trê',
+            email: 'johntre@example.com',
+            oldPassword: '123456',
+            password: '123123',
+        });
+
+        expect(updatedUser.password).toBe('123123');
+    });
+
+    it('should not be update the password without confirming the password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfileService.execute({
+                userId: user.id,
+                name: 'John Trê',
+                email: 'johntre@example.com',
+                password: '123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be update the password with wrong confirming password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfileService.execute({
+                userId: user.id,
+                name: 'John Trê',
+                email: 'johntre@example.com',
+                password: '123123',
+                oldPassword: '987654',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
---
+++ GoBarber-Backend/src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,82 @@
+import User from '@modules/users/infra/typeorm/entities/User';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+// import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+interface Request {
+    userId: string;
+    name: string;
+    email: string;
+    oldPassword?: string;
+    password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+
+        @inject('IHashProvider')
+        private hashProvider: IHashProvider,
+    ) {
+        this.usersRepository = usersRepository;
+    }
+
+    public async execute({
+        userId,
+        name,
+        email,
+        password,
+        oldPassword,
+    }: Request): Promise<User> {
+        const user = await this.usersRepository.findById(userId);
+
+        if (!user) {
+            throw new AppError('User not found');
+        }
+
+        const userWithUpdatedEmail = await this.usersRepository.findByEmail(
+            email,
+        );
+
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== userId) {
+            throw new AppError('Email already beeing used.');
+        }
+
+        if (password && !oldPassword) {
+            throw new AppError(
+                'To set a new password you should inform the old one.',
+            );
+        }
+
+        if (password && oldPassword) {
+            const checkPasswordMatch = await this.hashProvider.compareHash(
+                oldPassword,
+                user.password,
+            );
+
+            if (!checkPasswordMatch) {
+                throw new AppError('Passwords do not match!');
+            }
+        }
+
+        if (password) {
+            user.password = await this.hashProvider.generateHash(password);
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await this.usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export default UpdateProfileService;
